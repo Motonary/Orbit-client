f219880c01357daf121d15d297e15bdaef77279d
YUKI SHIROTA <39118010+rechtburg@users.noreply.github.com>
2019-10-15 05:01:21 +0900

    [Refactor] TopPage (#5)
    * create Button and Image
    
    * implements styled-components
    
    * delte file and remove css
    
    * rename TopPage
    
    * create Button story

---
+++ src/javascripts/components/__stories__/Button.story.tsx
@@ -0,0 +1,10 @@
+import { withKnobs } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+import Button from "../atoms/Button";
+
+storiesOf("Atoms/Button", module)
+  .addDecorator(withKnobs)
+  .add("Default", () => (
+    <Button >ボタン</Button>
+  ));
