d53d6b15e3924919b0b91153b223b1ea5d779c29
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-10-15 04:49:52 +0900

    implements styled-components

--- src/javascripts/components/pages/guests/top-page.tsx
+++ src/javascripts/components/pages/guests/top-page.tsx
@@ -1,51 +1,100 @@
-import * as React from 'react'
-import Alert from 'react-s-alert'
-import TopPageBtn from '../../atoms/buttons/toppage-btn'
-import TopPageLogo from '../../atoms/toppage-logo'
-import TopPagePlanet from '../../atoms/toppage-planet-img'
-import ToppageFormContainer from '../../molecules/toppage-form-container'
+import styled from "@emotion/styled"
+import * as React from "react"
 
-interface Props {
+import Alert from "react-s-alert"
+import Button from "../../atoms/Button"
+import Img from "../../atoms/Image"
+
+import { TopPageImgs } from "../../../constants/images"
+import TopLogo from "../../../images/index/logo.png"
+import Planet from "../../../images/index/top_earth.png"
+
+import SignInForm from "../../molecules/forms/signin-form"
+import SignUpForm from "../../molecules/forms/signup-form"
+
+interface Props extends React.Attributes {
   currentUser: any
   pathname: any
   history: any
 }
 
-interface State {
-  isSignIn: boolean
+const TopPage: React.FC<Props> = ({currentUser, pathname, history}) => {
+  const [isSignIn, setSignIn] = React.useState<boolean>(true);
+
+  function onTopPageButtonClick() {
+    setSignIn(!isSignIn);
+  }
+  
+  return (
+    <Root>
+      <_Button onClick={onTopPageButtonClick} >
+        {isSignIn ? (
+          <Img alt="sign-up" width="170px" src={TopPageImgs['signUp']} />
+        ) : (
+          <Img alt="sign-in" width="170px" src={TopPageImgs['signIn']} />
+        )}
+      </_Button>
+      <Alert />
+      <Logo>
+        <Img src={TopLogo} alt="logo" />
+      </Logo>
+      <SignForm>
+        {isSignIn ? <SignInForm history={history} /> : <SignUpForm history={history} />}
+      </SignForm>
+      <TopPlanet>
+        <Img src={Planet} alt="top page planet" />
+      </TopPlanet>
+    </Root>
+  )
 }
 
-export default class TopPage extends React.Component<Props, State> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      isSignIn: true,
-    }
-  }
+const Root = styled.div`
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+`;
 
-  onClickTopPageButton() {
-    const { isSignIn } = this.state
-    if (isSignIn) {
-      this.setState({ isSignIn: false })
-    } else if (!isSignIn) {
-      this.setState({ isSignIn: true })
-    } else {
-      throw new Error('Sorry, something went wrong. Please reload.') // 暫定の例外処理
-    }
-  }
+const _Button = styled(Button)`
+  display: block;
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  z-index: 1000;
+`;
 
-  render() {
-    const { isSignIn } = this.state
-    const { history } = this.props
+const Logo = styled.div`
+  margin: 10px auto;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  bottom: 505px;
 
-    return (
-      <div className="top-page-container">
-        <TopPageBtn isSignIn={isSignIn} onClick={this.onClickTopPageButton.bind(this)} />
-        <Alert />
-        <TopPageLogo />
-        <ToppageFormContainer isSignIn={isSignIn} history={history} />
-        <TopPagePlanet />
-      </div>
-    )
+  img {
+    margin: 0 auto;
+    max-width: 550px;
+    width: 100%;
   }
-}
+`;
+
+const SignForm = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  position: absolute;
+  bottom: 365px;
+`;
+
+const TopPlanet = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+
+  img {
+    margin: auto;
+    max-width: 900px;
+    width: 90%;
+  }
+`;
+
+export default TopPage
