91d3ad53709479073c4de9b9ba7b6c26fdbcf689
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-10-15 11:18:52 +0900

    formatted

--- src/javascripts/components/atoms/Image.tsx
+++ src/javascripts/components/atoms/Image.tsx
@@ -1,21 +1,15 @@
-import * as React from "react";
+import * as React from 'react'
 
 interface Props {
-  className?: string;
-  src: string;
-  alt: string;
-  height?: string;
-  width?: string;
+  className?: string
+  src: string
+  alt: string
+  height?: string
+  width?: string
 }
 
 const Image: React.FC<Props> = ({ className, src, alt, height, width }) => (
-  <img
-    className={className}
-    src={src}
-    alt={alt}
-    height={height}
-    width={width}
-  />
-);
+  <img className={className} src={src} alt={alt} height={height} width={width} />
+)
 
-export default Image;
+export default Image
