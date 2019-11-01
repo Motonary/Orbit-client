3dd5c899551f7710136dc30be0ad072f90f676e7
YUKI SHIROTA <39118010+rechtburg@users.noreply.github.com>
2019-11-01 10:35:53 +0900

    Revert "[Structure] Feature - remove jest once & restructure directory (#23)"
    This reverts commit 67e1d9ea750b5ef18b522d7d0ef13f82d190123f.

--- src/javascripts/components/atoms/FixedStarInList.tsx
+++ src/javascripts/components/atoms/FixedStarInList.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import * as React from "react";
 
-import Img from "./Image";
+import Img from "../atoms/Image";
 import { PlanetImgs } from "../../constants/ImagesUrl";
 
 interface Props {
