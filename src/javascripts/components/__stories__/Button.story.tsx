e2356dd6500b66d97b376b270ca426ac4a19e686
rechtburg <y.shirota.research.a.develop@gmail.com>
2019-11-01 14:38:33 +0900

    restructure

--- src/javascripts/components/__stories__/Button.story.tsx
+++
@@ -1,8 +0,0 @@
-import { withKnobs } from "@storybook/addon-knobs";
-import { storiesOf } from "@storybook/react";
-import * as React from "react";
-import Button from "../atoms/Button";
-
-storiesOf("Atoms/Button", module)
-  .addDecorator(withKnobs)
-  .add("Default", () => <Button>ボタン</Button>);
--- src/javascripts/constants/RootUrl.ts
+++
@@ -1,3 +0,0 @@
-// TODO: developmentとproductionで場合分け
-export const ROOT_URL = "http://localhost:3000";
-// export const ROOT_URL = 'http://52.69.76.248:3000'
