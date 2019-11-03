31ab3adb6a4d7409a349ede253cd81d7b87bf15f
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-11-03 18:07:12 +0900

    Fix top page images

--- src/javascripts/components/atoms/toppage-planet-img.tsx
+++ src/javascripts/components/atoms/toppage-planet-img.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const ImgPlanet = `${process.env.PUBLIC_URL}/images/index/top_earth.png`;
+const ImgPlanet = "/images/index/top_earth.png";
 
 const TopPagePlanet = () => (
   <div className="planet-img-container">
