31e6df37c59c35f4b7f3b68a9afbcb6375fe1432
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-11-07 16:27:37 +0900

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -39,5 +40,10 @@ module.exports = {
       },
     ],
   },
-  plugins: [new ExtractTextPlugin({ filename: "[name].css", allChunks: true })],
+  plugins: [
+    new ExtractTextPlugin({ filename: "[name].css", allChunks: true }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
 }
