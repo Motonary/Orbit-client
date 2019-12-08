0634dd77e8e7d1c7cd9e592f4e3301eb7e3676de
TaishiM <liverpool1407@g.ecc.u-tokyo.ac.jp>
2019-12-08 19:44:04 +0900

    Update production url

--- src/javascripts/constants/url.ts
+++ src/javascripts/constants/url.ts
@@ -1,6 +1,6 @@
 export const ROOT_URL =
   process.env.NODE_ENV === "production"
-    ? "http://52.69.76.248"
+    ? "https://api.the-orbit-app.com"
     : "http://localhost:3000";
 
 const CLOUD_FRONT_PROD_ROOT = "https://d111fnfgjqj6t5.cloudfront.net";
